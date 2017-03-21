a782f48af0431025afefd6232907b55d7938e061
Felipe Sandoval <fasandovalf@gmail.com>
2017-03-21 01:09:42 -0300

    Versión estable

---
+++ PilotoApp/src/app/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+*  This class represents the lazy loaded SignupComponent.
+*/
+
+@Component({
+  selector: 'signup-cmp',
+  templateUrl: 'signup.component.html'
+})
+
+export class SignupComponent { }
